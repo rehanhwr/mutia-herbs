ab2140e72b7b7bfc5b30d21834bfd46aeb4a972d
Rehan Hawari <rehan.hawari10@gmail.com>
2020-11-13 21:36:34 +0700

    justify center card section

--- src/Section.svelte
+++ src/Section.svelte
@@ -7,11 +7,15 @@
 
 {#each sectionData as sectionDatum (sectionDatum.id)}
     <div class="mb-2">
-        <h1 class="text-black mb-2 px-2 text-lg font-bold">{sectionDatum.title}</h1>
-        <div class="flex flex-wrap">
-            {#each sectionDatum.products as product (product.id)}
-                <Card title={product.title} description={product.description} price={product.price} />
-            {/each}
+        <div class="flex flex-col">
+            <div class="flex justify-center">
+                <h1 class="text-black mb-2 px-2 text-lg font-bold">{sectionDatum.title}</h1>
+            </div>
+            <div class="flex flex-wrap justify-center">
+                {#each sectionDatum.products as product (product.id)}
+                    <Card title={product.title} description={product.description} price={product.price} />
+                {/each}
+            </div>
         </div>
     </div>
 {/each}
